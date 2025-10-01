948c00d6ff863daf269d32ce91ec06ec0828a369
TobyMayn <tobiasmayn@gmail.com>
2025-10-01 16:00:18 +0200

    Create chip class

---
+++ Chip8Interpreter/Model/Chip.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Chip8Interpreter.Model
+{
+    public class Chip
+    {
+        protected byte[] _memory = new byte[0x1000];
+    }
+}
