47a23cfc084a725ff0ee1f76f81f3168f0300e82
TobyMayn <tobiasmayn@gmail.com>
2025-10-03 12:19:36 +0200

    Create keyboard class

---
+++ Chip8Interpreter/Model/Keyboard.cs
@@ -0,0 +1,12 @@
+﻿namespace Chip8Interpreter.Model
+{
+    public class Keyboard
+    {
+        private readonly string[,] layout = new string[4, 4] {
+            {"1", "2", "3", "C"},
+            {"4", "5", "6", "D"},
+            {"7", "8", "9", "E"},
+            {"A", "0", "B", "F"}
+        };
+    }
+}
