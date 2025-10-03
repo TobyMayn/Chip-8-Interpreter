db8ef7930fce24f2d1e2a239b49a6d4c2eacf53f
TobyMayn <tobiasmayn@gmail.com>
2025-10-03 15:08:57 +0200

    refactor ClearDisplay method

--- Chip8Interpreter/Models/Display.cs
+++ Chip8Interpreter/Models/Display.cs
@@ -6,9 +6,6 @@
         public const int SCREEN_HEIGHT = 32;
         private bool[,] display = new bool[SCREEN_WIDTH, SCREEN_HEIGHT];
 
-        public void ClearDisplay()
-        {
-            this.display = new bool[SCREEN_WIDTH, SCREEN_HEIGHT];
-        }
+        public void ClearDisplay() => this.display = new bool[SCREEN_WIDTH, SCREEN_HEIGHT];
     }
 }
