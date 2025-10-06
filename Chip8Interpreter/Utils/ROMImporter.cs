7a3d0014a10dc510010e53918e302ab0f1c3318b
TobyMayn <tobiasmayn@gmail.com>
2025-10-06 11:29:12 +0200

    implement class to read in ROM files

---
+++ Chip8Interpreter/Utils/ROMImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Chip8Interpreter.Utils
+{
+    public class ROMImporter
+    {
+        private readonly byte[] _rom;
+
+        public ROMImporter(string filePath)
+        {
+            this._rom = ReadROMFile(filePath);
+        }
+
+        private byte[] ReadROMFile(string filePath)
+        {
+            return File.ReadAllBytes(filePath);
+        }
+
+        public byte[] GetROMFile() 
+        { 
+            return this._rom;
+        }
+    }
+}
