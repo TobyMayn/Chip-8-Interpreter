635d407a3ebb317063810d20cee019cb45d49f1b
TobyMayn <tobiasmayn@gmail.com>
2025-10-07 15:45:22 +0200

    create placeholder file for cpu tests

---
+++ Chip8InterpreterTests/Models/CPUTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chip8Interpreter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Interpreter.Models.Tests
+{
+    [TestClass()]
+    public class CPUTests
+    {
+        [TestMethod()]
+        public void CPUTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ResetCPUTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ClearRegistersTest()
+        {
+
+        }
+    }
+}
