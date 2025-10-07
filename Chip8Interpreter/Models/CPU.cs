1139fc3ea3c55bfe3adc405fdd2c18fad53d43f8
TobyMayn <tobiasmayn@gmail.com>
2025-10-07 15:45:05 +0200

    Implement register and pc related operations

--- Chip8Interpreter/Models/CPU.cs
+++ Chip8Interpreter/Models/CPU.cs
@@ -13,7 +13,7 @@ namespace Chip8Interpreter.Models
         private byte _delayTimer = 60;
         private byte _soundTimer = 60;
 
-        private int[] _registers = new int[16];
+        private byte[] _registers = new byte[16];
 
         public CPU() {
             this.ResetCPU();
@@ -38,6 +38,75 @@ namespace Chip8Interpreter.Models
             {
                 this._registers[i] = 0x0;
             }
+        }
+
+        private void SetRegister(byte register, byte value)
+        {
+            this._registers[register] = value;
+        }
+
+        private void AddToRegister(byte register, byte value)
+        {
+            this._registers[register] += value;
+        }
+
+        /// <summary>
+        /// Sets the value of registerA to the value of registerB
+        /// </summary>
+        /// <param name="registerA"></param>
+        /// <param name="registerB"></param>
+        private void SetRegisterToValueOfRegister(byte registerA, byte registerB)
+        {
+            this._registers[registerA] = this._registers[registerB];
+        }
+
+        /// <summary>
+        /// Performs an OR operation between registerA and registerB. 
+        /// Then sets the registerA to the value of the operation.
+        /// </summary>
+        /// <param name="registerA"></param>
+        /// <param name="registerB"></param>
+        private void RegisterOperationOR(byte registerA, byte registerB)
+        {
+            this._registers[registerA] = (byte)(this._registers[registerA] | this._registers[registerB]);
+        }
+
+        /// <summary>
+        /// Performs an AND operation between registerA and registerB. 
+        /// Then sets the registerA to the value of the operation.
+        /// </summary>
+        /// <param name="registerA"></param>
+        /// <param name="registerB"></param>
+        private void RegisterOperationAND(byte registerA, byte registerB)
+        {
+            this._registers[registerA] = (byte)(this._registers[registerA] & this._registers[registerB]);
+        }
+
+        /// <summary>
+        /// Performs a XOR operation between registerA and registerB. 
+        /// Then sets the registerA to the value of the operation.
+        /// </summary>
+        /// <param name="registerA"></param>
+        /// <param name="registerB"></param>
+        private void RegisterOperationXOR(byte registerA, byte registerB)
+        {
+            this._registers[registerA] = (byte)(this._registers[registerA] ^ this._registers[registerB]);
+        }
+
+        private void SetPC(ushort value)
+        {
+            this._pc = value;
+        }
+
+        private void GoToSubroutine(ushort memoryLocation)
+        {
+            this._stack.Push(this._pc);
+            this.SetPC(memoryLocation);
+        }
+
+        private void ReturnFromSubroutine()
+        {
+            this.SetPC(this._stack.Pop());
         }
     }
 }
