c9ecbfc9de83df782912520c83514684eefe3a6d
TobyMayn <tobiasmayn@gmail.com>
2025-10-23 20:09:43 +0200

    implement decoding of instruction to make ibm logo rom run

--- Chip8Interpreter/Models/Emulator.cs
+++ Chip8Interpreter/Models/Emulator.cs
@@ -1,6 +1,8 @@
 ﻿namespace Chip8Interpreter.Models
 {
     using Chip8Interpreter.Utils;
+    using System.Runtime.CompilerServices;
+
     public class Emulator
     {
         private CPU _cpu;
@@ -24,19 +26,25 @@
                 instruction = this._memory.GetInstruction(this._cpu.Pc);
                 this._cpu.Pc += 2;
                 this.Decode(instruction);
-
-
             }
          
         }
 
         private void Decode(ushort instruction)
         {
-            switch (instruction)
+            byte registerA, registerB, value;
+            switch (instruction & 0xF000)
             {
                 case 0x0:
+                    switch (instruction)
+                    {
+                        case 0x00E0:
+                            this._display.ClearDisplay();
+                            break;
+                    }
                     break;
                 case 0x1:
+                    this._cpu.Pc = (ushort)(instruction & 0x0FFF);
                     break;
                 case 0x2:
                     break;
@@ -47,20 +55,31 @@
                 case 0x5:
                     break;
                 case 0x6:
+                    registerA = (byte)(instruction & 0x0F00);
+                    value = (byte)(instruction & 0x00FF);
+                    this._cpu.SetRegister(registerA, value);
                     break;
                 case 0x7:
+                    registerA = (byte)(instruction & 0x0F00);
+                    value = (byte)(instruction & 0x00FF);
+                    this._cpu.AddToRegister(registerA, value);
                     break;
                 case 0x8:
                     break;
                 case 0x9:
                     break;
                 case 0xA:
+                    this._cpu.Idr = (ushort)(instruction & 0x0FFF);
                     break;
                 case 0xB:
                     break;
                 case 0xC:
                     break;
                 case 0xD:
+                    registerA = (byte)(instruction & 0x0F00);
+                    registerB = (byte)(instruction & 0x00F0);
+                    value = (byte)(instruction & 0x000F);
+                    Draw(registerA, registerB, value);
                     break;
                 case 0xE:
                     break;
